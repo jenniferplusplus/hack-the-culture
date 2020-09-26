4bea8ac447af6f40f1a101bda630ccf6b86bc2c7
Jennifer Moore <jenniferplusplus@gmail.com>
2020-09-25 22:32:42 -0500

    scaffolded vuetify based ui

---
+++ ui/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app id="app">
+    <v-app-bar id="nav" app>
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+    </v-app-bar>
+    <router-view/>
+  </v-app>
+</template>
---
+++ ui/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-main>
+    <h1>This is an about page</h1>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: 'about'
+}
+</script>
